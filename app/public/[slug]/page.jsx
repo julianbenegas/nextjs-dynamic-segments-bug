a495916d20d5fba5fa310a97a0381ba179da21ce
Julian Benegas <julianbenegas99@gmail.com>
2024-10-23 17:05:01 -0300

    init

---
+++ app/public/[slug]/page.jsx
@@ -0,0 +1,17 @@
+const Page = async ({ params: _params }) => {
+  const params = await _params;
+  console.log(params);
+  return <div>{JSON.stringify(params)}</div>;
+};
+
+export const generateMetadata = async ({ params: _params }) => {
+  const params = await _params;
+  console.log(params);
+  return { title: params.slug };
+};
+
+export const generateStaticParams = async () => {
+  return [];
+};
+
+export default Page;
---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  console.log(url.pathname);
+
+  if (url.pathname.startsWith("/public")) {
+    notFound();
+  }
+
+  if (url.pathname !== "/") {
+    url.pathname = "/public" + url.pathname;
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/:path*",
+};
